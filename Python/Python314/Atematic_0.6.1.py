130241a5fd5bd7d1acffdb4de18f0b705e164a0d
Declan <fortuneahisu07@gmail.com>
2026-05-09 19:18:55 +0100

    Perfected the cartesian positioning logic

--- Python/Python314/Atematic_0.6.1.py
+++ Python/Python314/Atematic_0.6.1.py
@@ -1,46 +1,44 @@
 """
 Motion simulation project
 """
-from math import sin, cos, pi
+from math import sin, cos
 import cmath
+import numpy as np
 
 def motion_simulator():
     """
     Motion physics simulator
     """
+    pi = 3.1415926535897932384626433832795
+    position = np.array([0.0, 0.0])
+    step_length = np.array([1.0, 1.0])
+    velocity = np.array([0, 0])
+    turn_angle = pi/4
+    angle = 0
     while True:
-        position = np.array([0.0, 0.0])
-        dt = np.array([0.001, 0.001])
-        step = np.array([0.0, 0.0])
-        turn_angle = 1
-        angle = 0
-        radians = angle/180 * pi
-        control = input('Control: ').lower()
-        if control == 'w':
-            step += dt
-        elif control == 's':
-            step -= dt
-        elif control == 'a':
-            angle -= turn_angle
-        elif control == 'd':
-            angle += turn_angle
-        else:
-            print('Huh, 😕')
-        if angle >= 360 or angle <= -360:
-            angle = 0
-        velocity_x = sin(radians) * step
-        velocity_y = cos(radians) * step
-        velocity = cmath.sqrt(velocity_x ** 2 + velocity_y ** 2)
-        x += velocity_x
-        y += velocity_y
-        print(x)
-        print(y)
-        print(radians)
-        print(angle)
-        print(radians)
-        print(step)
-        print(velocity_x)
-        print(velocity_y)
+        try:
+            if (angle + turn_angle) > 2*pi or (angle - turn_angle) < -2*pi:
+                angle = 0
+            trig_equalizers = np.array([sin(angle), cos(angle)])
+            control = input('Control: ').lower()
+            if control == 'w':
+                position += step_length * trig_equalizers
+                continue
+            elif control == 's':
+                position -= step_length * trig_equalizers
+                continue
+            elif control == 'a':
+                angle -= turn_angle
+                continue
+            elif control == 'd':
+                angle += turn_angle
+                continue
+            elif control == 'break':
+                break
+        except ValueError:
+            print('Nope')
+        except TypeError:
+            print('Nope')
 
 
 
