353e94276de1e8aa245b648a9968163adbcfb30e
Declan <fortuneahisu07@gmail.com>
2026-05-22 13:50:26 +0100

    Continued refactoring

--- HTML_CSS_JS/Ackerman series/fvic_prototype.js
+++ HTML_CSS_JS/Ackerman series/fvic_prototype.js
@@ -1,16 +1,7 @@
-document.querySelector(".nav-links").addEventListener("mouseenter", () => {
-    navButton.textContent = "Explore";
-});
+// document.querySelector(".nav-links").addEventListener("mouseenter", () => {
+//     navButton.textContent = "Explore";
+// });
 
-document.querySelector("#nav-button").addEventListener("mouseleave", () => {
-    navButton.textContent = "Navigate";
-});
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-    anchor.addEventListener("click", function (scroll) {
-        scroll.preventDefault();
-
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior: "smooth",
-        });
-    });
-});
+// document.querySelector("#nav-links").addEventListener("mouseleave", () => {
+//     navButton.textContent = "Navigate";
+// });
